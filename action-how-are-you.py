cccca26d671ea61996aef38aea46317f6cebab9d
Jan Bilek <j.bilek@mytaxi.com>
2019-01-27 15:53:04 +0100

    update

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
 
+import datetime
+
 INTENT_HOW_ARE_YOU = "hobil:how_are_you"
 INTENT_COLOR = "hobil:color"
+INTENT_TIME = "hobil:time"
 
 
 def main():
     with Hermes("localhost:1883") as h:
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
          .subscribe_intent(INTENT_COLOR, color_callback) \
+         .subscribe_intent(INTENT_TIME, time_callback) \
          .start()
 
 
@@ -23,6 +27,13 @@ def how_are_you_callback(hermes, intent_message):
 def color_callback(hermes, intent_message):
     session_id = intent_message.session_id
     response = "Yellow."
+    hermes.publish_end_session(session_id, response)
+
+def time_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    time = datetime.datetime.now()
+    response = "It's " + str(time.hour) + " " + str(time.minute)
+    #response = "Yellow."
     hermes.publish_end_session(session_id, response)
 
 
